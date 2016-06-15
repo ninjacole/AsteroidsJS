9b9894c87398855a643810af27b69a249a21a6b7
matt <vartanian.matt@gmail.com>
2016-06-14 21:42:09 -0400

    augmented asteroids_game with key object

---
+++ Scripts/asteroids-key-helper.js
@@ -0,0 +1,25 @@
+﻿var ASTEROIDS_GAME = (function (asteroids_game) {
+    asteroids_game.key = {
+        _pressed: {},
+
+        LEFT: 37,
+        UP: 38,
+        RIGHT: 39,
+        DOWN: 40,
+        SPACE: 32,
+
+        isDown: function (keyCode) {
+            return this._pressed[keyCode];
+        },
+
+        onKeyDown: function (event) {
+            this._pressed[event.keyCode] = true;
+        },
+
+        onKeyUp: function (event) {
+            delete this._pressed[event.keyCode];
+        }
+    };
+    return asteroids_game;
+
+})(ASTEROIDS_GAME);
